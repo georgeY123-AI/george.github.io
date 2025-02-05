2bf92f9ab8db98754ad463ac4df0fa753cc8651e
georgeY123-AI <152098244+georgeY123-AI@users.noreply.github.com>
2025-02-05 14:10:35 +0200

    undo

--- src/data/ProjectData.js
+++ src/data/ProjectData.js
@@ -6,7 +6,8 @@ export const ProjectList = [
     description:
       "A cross-platform mobile game (in closed testing) where players try to find the missing pieces of a story.",
     tech_stack: ["React Native", "Google Cloud", "NextJs", "TailwindCSS"],
-    github_url: ""
+    github_url: "",
+    demo_url: "https://youknowtheending.net/",
   },
   {
     img: "/renr-responsive.png",
@@ -14,7 +15,8 @@ export const ProjectList = [
     description:
       "A safety application for the Renewable Resources Dept. of BCIT",
     tech_stack: ["React", "Firebase"],
-    github_url: "" //renr-bcit.web.app/",
+    github_url: "",
+    demo_url: "https://renr-bcit.web.app/",
   },
   {
     img: "/mymind-responsive.png",
@@ -22,7 +24,8 @@ export const ProjectList = [
     description:
       "An app where users can purchase online therapy sessions and chat with their therapists.",
     tech_stack: ["HTML", "CSS", "NodeJs", "MongoDB", "SocketIo"],
-    github_url: ""
+    github_url: "https://github.com/salesp07/MyMind---Copy",
+    demo_url: "https://mymindweb.vercel.app/",
   },
   {
     img: "/pokedex-responsive.png",
@@ -30,7 +33,8 @@ export const ProjectList = [
     description:
       "A pokedex app that displays pokemon data, plays OSTs and makes pokemons dance.",
     tech_stack: ["ReactJs", "NodeJs", "MongoDB"],
-    github_url: " "
+    github_url: "https://github.com/salesp07/Pokedex-ReactApp",
+    demo_url: "https://pokedex-salesp07.netlify.app/",
   },
   {
     img: "/jobDetect.png",
@@ -38,7 +42,8 @@ export const ProjectList = [
     description:
       "An app to detect if a job posting is a scam or not using its description.",
     tech_stack: ["Python", "Flask", "NodeJs", "MongoDB"],
-    github_url: ""
+    github_url: "https://github.com/salesp07/JobDetectNode",
+    demo_url: "https://jobdetective.onrender.com/",
   },
 ];
 
@@ -120,6 +125,10 @@ export const stackList = [
   {
     img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/figma/figma-original.svg",
     name: "Figma",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/tailwindcss/tailwindcss-plain.svg",
+    name: "TailwindCSS",
   },
   {
     img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/vscode/vscode-original.svg",
