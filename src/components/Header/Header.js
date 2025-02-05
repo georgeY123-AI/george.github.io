af140691704f58d31850427f9e67087072cabff0
georgeY123-AI <152098244+georgeY123-AI@users.noreply.github.com>
2025-02-05 13:59:10 +0200

    Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -25,7 +25,7 @@ const Header = ({ toggle }) => {
         <NavBtn>
           <a
             className="btn PrimaryBtn"
-            href="https://www.linkedin.com/in/george-ai/"
+            href="https://drive.google.com/file/d/1Zq-9xfgl7R21-KhT_wH_jGW-zfhBpho-/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
           >
