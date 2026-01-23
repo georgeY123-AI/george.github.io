d3f84f34ebef6d2235d81febc243ff8ef4fbd9ea
goergeY123-AI <george.youssef077@gmail.com>
2026-01-24 00:41:29 +0200

    my resume update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -25,7 +25,7 @@ const Header = ({ toggle }) => {
         <NavBtn>
           <a
             className="btn PrimaryBtn"
-            href="https://drive.google.com/file/d/1gnhcNOvlnL3AnDSux4cjmLb8DEowJKFZ/view?usp=drive_link"
+            href="https://drive.google.com/file/d/1BbKAInmJVvf0a1okgS7TfKJo4t0zLEt7/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
           >
